c9a4ec9d49fc1c79ee0833e27d30f1092eabe4f9
ArturDokhno <artur.dohno@icloud.com>
2022-02-03 13:55:04 +0500

    Lecture 1: Getting started with SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+                .foregroundColor(.red)
+            Text("Hello world")
+                .foregroundColor(.blue)
+        }
+        .padding()
     }
 }
 
